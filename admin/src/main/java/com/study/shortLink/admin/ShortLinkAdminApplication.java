81c1b4cf72ea5c8d70ff0d2a5264adbad4bf3e3a
wearingcucumber <257654131@qq.com>
2024-02-04 00:54:48 +0800

    feature: 引入微服务框架：nacos、openfeign 添加依赖修改配置文件

--- admin/src/main/java/com/study/shortLink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/com/study/shortLink/admin/ShortLinkAdminApplication.java
@@ -3,8 +3,13 @@ package com.study.shortLink.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @MapperScan("com.study.shortLink.admin.dao.mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients("com.study.shortLink.admin.remote")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkAdminApplication.class,args);
--- project/src/main/java/com/study/shortLink/project/ShortLinkApplication.java
+++ project/src/main/java/com/study/shortLink/project/ShortLinkApplication.java
@@ -3,9 +3,11 @@ package com.study.shortLink.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.study.shortLink.project.dao.mapper")
+@EnableDiscoveryClient
 public class ShortLinkApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkApplication.class,args);
